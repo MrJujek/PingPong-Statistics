1c25f2e0104e53ea2af9dff10c76ea2f94016af5
MrJujek <julian.dworzycki@gmail.com>
2022-09-22 18:41:11 +0200

    chart fixed

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
-// function drawChart(players, points, chart_name) {
-//     const wykres = new Chart(document.getElementById(chart_name), {
-//         type: 'bar',
-//         data: {
-//             labels: players,
-//             datasets: [{
-//                 data: points,
-//                 label: "Points",
-//                 fill: true,    //false,
-//                 tension: 0.3,
-//             }]
-//         },
-//         options: {
-//             title: {
-//                 display: true,
-//                 text: 'Points'
-//             }
-//         },
-//     });
+function drawChart(players, points, chart_name) {
+    const wykres = new Chart(document.getElementById(chart_name), {
+        type: 'bar',
+        data: {
+            labels: players,
+            datasets: [{
+                data: points,
+                label: "Points",
+                fill: true,    //false,
+                tension: 0.3,
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Points'
+            }
+        },
+    });
 
-//     return wykres;
-// }
+    return wykres;
+}
+
 let players = ['julian', 'radek', 'kamil', 'tomek']
 let points = [5, 3, 12, 1]
-let chart_name = "Statistics"
+let chart_name = "statistics"
 
-const wykres = new Chart(document.getElementById(chart_name), {
-    type: 'bar',
-    data: {
-        labels: players,
-        datasets: [{
-            data: points,
-            label: "Points",
-            fill: true,    //false,
-            tension: 0.3,
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Points'
-        }
-    },
-});
+drawChart(players, points, chart_name)
